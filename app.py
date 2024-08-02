4c0627732a31727f66accf9c49918d4bec247618
ManavDurgapal <126187285+ManavDurgapal@users.noreply.github.com>
2024-08-02 23:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import gdown
 import os
 
 # Google Drive URL of the pickle file
-url = 'https://drive.google.com/uc?id=YOUR_FILE_ID'
+url = 'https://drive.google.com/file/d/1XTIuOLtI_0iYHXLj4DL8Hbm9kEyCQPJT/view?usp=sharing'
 output = 'df_with_embeddings.pkl'
 
 # Download the pickle file from Google Drive
